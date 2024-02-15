4ea2cd4c6892a1186ae49042bc216dd8389167c4
SebastianBuftea <sebibuftea85@gmail.com>
2024-02-15 16:26:14 +0100

    [UPDATE] create table

---
+++ database/migrations/2024_02_15_143453_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('agency', 50);
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->smallInteger('number_train')->unsigned();
+            $table->tinyInteger('number_wagons')->nullable();
+            $table->boolean('on_time')->nullable();
+            $table->boolean('delated')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
